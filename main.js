cc918d6c69a8546e19165de630829529ab9076d4
Lore melo <143742218+loremello@users.noreply.github.com>
2024-10-01 11:46:20 -0300

    hhghgjhgjgj

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-function soma(num1,num2){
-    return num1+num2
-}
-console.log(soma(3,6));
+// function soma(num1,num2){
+//     return num1+num2
+// }
+// console.log(soma(3,6));
 
-function area(num1){
-    return num1**2;
-}
-console.log(area(4))
-let areaTrapezio=function(num1,num2,num3) {
-    return[num1+num2]*num3/2;
-}
-console.log(areaTrapezio(3,4,6))
+// function area(num1){
+//     return num1**2;
+// }
+// console.log(area(4))
+// let areaTrapezio=function(num1,num2,num3) {
+//     return[num1+num2]*num3/2;
+// }
+// console.log(areaTrapezio(3,4,6))
 
-let media de valores= prompt('media de valores?')
-document.write(meia de valores)
-parseInt(prompt('media de valores?'))
+// let media de valores= prompt('media de valores?')
+// document.write(meia de valores)
+// parseInt(prompt('media de valores?'))
+let nomes=['Lucas','João','Lorena','Maria','Arthur','Julia','Nicolas','Felipe','Luiz','Gabriel','Marina','Lais','Mel','Monise','Caio','Aaron','Vinicius','Danilo','Gabriela','Geovana'];
+
+let i=0;
+while(i<10 ) {
+    console.log(i);
+    i=i+1;
+}
